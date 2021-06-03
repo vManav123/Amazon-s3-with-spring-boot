4a38fe38e5ccb64d8552e77093d6f7f8971e625c
Manav Verma <vmanav1999@gmail.com>
2021-06-03 21:31:22 +0530

    Crud Operation over AWS S3 via spring-boot-rest-api.

---
+++ src/main/java/com/aws/s3/controller/StorageController.java
@@ -0,0 +1,43 @@
+package com.aws.s3.controller;
+
+import com.aws.s3.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+public class StorageController {
+
+    // *---------- Autowiring reference variables ---------*
+    @Autowired
+    private StorageService service;
+
+    // *----------- Upload file -----------*
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    // *----------- Download file -----------*
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    // *----------- Delete file -----------*
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
